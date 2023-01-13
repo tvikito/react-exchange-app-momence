c83150a6cc2b85f2aa0f484c11a2973dbf817e0e
Tomas Vykoukal <vykoukal.tom@gmail.com>
2023-01-13 17:17:06 +0100

    remove webvitals

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './reset.css'
 import './index.css'
 import App from './App'
-import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -11,5 +10,3 @@ root.render(
     <App />
   </React.StrictMode>,
 )
-
-reportWebVitals()
